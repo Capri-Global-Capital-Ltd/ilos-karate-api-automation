63fdb5ef46debe789eb5caaa6781b51c1d44d226
Ashish Singh <ashish.singh3@capriglobal.in>
2025-01-06 18:52:49 +0530

    add

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -15,8 +15,8 @@ function() {
     TEST_DATA = 'file:src/test/resources/stg/';
     TEST_DATA1 = 'file:src/test/resources/';
     TEST_DATA1 = 'file:src/test/resources/';
-    OBJ_ID='6768e9f17a11c691c18ee1b1';
-    APP_ID='911039_83';
+    OBJ_ID='677bd48b347d94f7e5b121b7';
+    APP_ID='91103_1';
     APP_ID1 = APP_ID+"_1";
     }
     else if (env == 'prod') {
